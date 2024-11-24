f384033163360c4f7f1b9f167de76935218f5a64
Gideon Boakye <122945393+yungProg@users.noreply.github.com>
2024-11-24 20:06:59 +0000

    Overwrite `to_s` method.
    This provides info when printing an instance without
    explicitly calling a method

--- instance_methods_and_variables.rb
+++ instance_methods_and_variables.rb
@@ -37,13 +37,19 @@ end
 class MyCar < Vehicle
   include Open
   NUMBER_OF_DOORS = 4
-  def info
-    puts "Your #{@color} #{year} #{@model} is moving at #{@current_speed} mph. \n #{openable}"
+  def to_s
+    "Your #{@color} #{@year} #{@model} is moving at #{@current_speed} mph. \n#{openable}"
   end
 end
 
 class MyTruck < Vehicle
   NUMBER_OF_DOORS = 2
+  def to_s
+    "#{@year}, #{@color} #{@model} is moving at #{@current_speed} mph."
+  end
 end
 
-puts MyCar.ancestors
+car = MyCar.new(2023, "Black", "Camry")
+truck = MyTruck.new(2024, "Yellow", "Man")
+puts car
+puts truck
